f5d5812d86f108bb073f6c3cbb400f8edd2a4194
MortenFeldt <mortenguy@hotmail.com>
2021-10-03 09:41:58 +0200

    Initialize project code

---
+++ src/main/java/dk/mortenfeldt/project/LaundrybookingsystemApplication.java
@@ -0,0 +1,13 @@
+package dk.mortenfeldt.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundrybookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundrybookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/mortenfeldt/project/LaundrybookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.mortenfeldt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundrybookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
